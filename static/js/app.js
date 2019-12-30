43bc5e6813c4d5294b2ee9034d918a8cd92ea020
Matt Kennedy <mjknj18@gmail.com>
2019-12-30 08:54:28 -0500

    Updated JavaScript Code.

--- static/js/app.js
+++ static/js/app.js
@@ -8,3 +8,22 @@ d3.json("https://raw.githubusercontent.com/mjknj18/Belly-Button-Biodiversity-Ana
 
 console.log(names)
 
+// Define Variable for Sample MetaData
+var metadata = []
+
+// Import Sample MetaData from JSON File
+d3.json("https://raw.githubusercontent.com/mjknj18/Belly-Button-Biodiversity-Analysis/master/data/samples.json").then(function(data) {
+  data["metadata"].forEach(item => metadata.push(item));
+});
+
+console.log(metadata)
+
+// Define Variable for Sample Information
+var information = []
+
+// Import Sample Information from JSON File
+d3.json("https://raw.githubusercontent.com/mjknj18/Belly-Button-Biodiversity-Analysis/master/data/samples.json").then(function(data) {
+  data["samples"].forEach(item => information.push(item));
+});
+
+console.log(information)
