5f120063806d4b18fe0ef96cdf4339196ab19490
Matt Kennedy <mjknj18@gmail.com>
2019-12-30 08:31:07 -0500

    Updated JavaScript Code.

--- static/js/app.js
+++ static/js/app.js
@@ -1,19 +1,10 @@
-// d3.json("https://github.com/mjknj18/Belly-Button-Biodiversity-Analysis/blob/master/data/samples.json", function(data){
-//     console.log(data)})
+// Define Variable for Sample Names
+var names = []
 
-// var data = fetch("https://github.com/mjknj18/Belly-Button-Biodiversity-Analysis/blob/master/data/samples.json")
-// console.log(JSON.stringify(data))
+// Import Sample Names from JSON File
+d3.json("https://raw.githubusercontent.com/mjknj18/Belly-Button-Biodiversity-Analysis/master/data/samples.json").then(function(data) {
+  data["names"].forEach(item => names.push(item));
+});
 
-fetch('https://github.com/mjknj18/Belly-Button-Biodiversity-Analysis/blob/master/data/samples.json')
-  .then(response => {
-    console.log(response.json())
-    return response.json()
-  })
-  .then(data => {
-    // Work with JSON data here
-    console.log(data)
-  })
-  .catch(err => {
-    // Do something for an error here
-    console.log('error')
-  })
+console.log(names)
+
