a7091b50f0f364cdc52b79cfecc4968782a803b6
Matt Kennedy <mjknj18@gmail.com>
2019-12-29 20:01:40 -0500

    Updated JavaScript Code.

--- static/js/app.js
+++ static/js/app.js
@@ -1,2 +1,19 @@
-d3.json("https://github.com/mjknj18/Belly-Button-Biodiversity-Analysis/blob/master/data/samples.json", function(data){
-    console.log(data)})
+// d3.json("https://github.com/mjknj18/Belly-Button-Biodiversity-Analysis/blob/master/data/samples.json", function(data){
+//     console.log(data)})
+
+// var data = fetch("https://github.com/mjknj18/Belly-Button-Biodiversity-Analysis/blob/master/data/samples.json")
+// console.log(JSON.stringify(data))
+
+fetch('https://github.com/mjknj18/Belly-Button-Biodiversity-Analysis/blob/master/data/samples.json')
+  .then(response => {
+    console.log(response.json())
+    return response.json()
+  })
+  .then(data => {
+    // Work with JSON data here
+    console.log(data)
+  })
+  .catch(err => {
+    // Do something for an error here
+    console.log('error')
+  })
